b3568fdfbbf0ce3add6e3074c18b0381b021d9bf
woaiwodeguo <821432386@qq.com>
2021-12-13 01:19:08 +0800

    模仿axios请求数据

--- src/store/modules/products.js
+++ src/store/modules/products.js
@@ -22,23 +22,23 @@ export default {
         }
     },
     actions: {
-        //    async getAllProducts({commit}){
-        //         try {
-        //             const res = await Axios.get('/api/products');
-        //             const results = res.data.results;
-        //             commit('getAllProducts',results)
-        //         } catch (error) {
-        //             console.log(error);
-        //         }
-        //     },
-        getAllProducts({ commit }) {
-            const results = [
-                { id: 1, title: 'javaScript百炼成仙', price: 77, inventory: 7999 },
-                { id: 2, title: 'javaScript之王者归来', price: 66, inventory: 6999 },
-                { id: 3, title: 'javaScript忍者禁书', price: 55, inventory: 5999 },
-            ]
-            commit('getAllProducts', results)
-        }
+           async getAllProducts({commit}){
+                try {
+                    const res = await Axios.get('/api/products');
+                    const results = res.data.results;
+                    commit('getAllProducts',results)
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+        // getAllProducts({ commit }) {
+        //     const results = [
+        //         { id: 1, title: 'javaScript百炼成仙', price: 77, inventory: 7999 },
+        //         { id: 2, title: 'javaScript之王者归来', price: 66, inventory: 6999 },
+        //         { id: 3, title: 'javaScript忍者禁书', price: 55, inventory: 5999 },
+        //     ]
+        //     commit('getAllProducts', results)
+        // }
 
     }
 }
